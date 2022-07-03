8cbeed5f225bc0b0e5e25ca2717bc6d50adc6b2a
adhamniyozboyev <adhamniyozboyev77@gmail.com>
2022-07-03 17:12:49 +0500

    if06

--- bin/if06.dart
+++ bin/if06.dart
@@ -5,6 +5,22 @@ Args:
     n: Five-digit number.
 Returns:
     int: return answer. 
-*/
+*/int func(n){
+  int a=n%10;
+  int b=n~/10%10;
+  int c=n~/10%100~/10;
+  int d=n~/10~/100%10;
+  int e=n~/10~/100~/10;
+  if (a>b&&b>c&&c>d&&d>e){
+    return 1;
+  }else if(a<b&&b>c&&c>d&&d>e){
+    return 2;
+  }else if(a<b&&b<c&&c>d&&d>e){
+    return 3;
+  }else if (a<b&&b<c&&c<d&&d>e){
+    return 4;
+  }return 5;
+}
 void main() {
+  print(func(84635));
 }
