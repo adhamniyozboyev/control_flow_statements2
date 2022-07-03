fc14e0e7b7e563884da9ee4b6f3fdf51258a43aa
adhamniyozboyev <adhamniyozboyev77@gmail.com>
2022-07-03 16:55:02 +0500

    if05

--- bin/if05.dart
+++ bin/if05.dart
@@ -6,6 +6,21 @@ Args:
 Returns:
     int: return answer.
 */
-
-void main() {
+int func(n){
+  int a=n%10;
+  int b=n~/10%10;
+  int c=n~/10%100~/10;
+  int d=n~/10~/100%10;
+  int e=n~/10~/100~/10;
+  if(a>b&&b>c&&c>d&&d>e){
+    return a;
+  }else if (a<b&&b>c&&c>d&&d>e){
+    return b;
+  }else if (a>b&&b<c&&c>d&&d>e){
+    return c;
+  }else if(a<b&&b<c&&c<d&&d>e){
+    return d;
+  }return e;
+}
+void main() {print(func(34857));
 }
