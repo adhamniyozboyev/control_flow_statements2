881b0734b35f33fe8f3a084928b7f6fa8595252e
adhamniyozboyev <adhamniyozboyev77@gmail.com>
2022-07-03 16:30:19 +0500

    if04

--- bin/if04.dart
+++ bin/if04.dart
@@ -7,6 +7,14 @@ Args:
 Returns:
     int: return answer. 
 */
-
+int func(int a, int b){
+ if(a==b){
+  return 0;
+ }else if(a>b){
+  return a;
+ }
+ return b;
+}
 void main() {
+  print(func(7,5));
 }
