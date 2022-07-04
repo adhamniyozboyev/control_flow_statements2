ce7975e5abd4d0edd2bf5cf17c9ea6e151433e0c
adhamniyozboyev <adhamniyozboyev77@gmail.com>
2022-07-04 14:31:57 +0500

    if06

--- bin/if06.dart
+++ bin/if06.dart
@@ -11,16 +11,16 @@ Returns:
   int c=n~/10%100~/10;
   int d=n~/10~/100%10;
   int e=n~/10~/100~/10;
-  if (a>b&&b>c&&c>d&&d>e){
+  if (a>b&&a>c&&a>d&&a>e){
     return 1;
-  }else if(a<b&&b>c&&c>d&&d>e){
+  }else if(b>a&&b>c&&b>d&&b>e){
     return 2;
-  }else if(a<b&&b<c&&c>d&&d>e){
+  }else if(c>a&&c>b&&c>d&&c>e){
     return 3;
-  }else if (a<b&&b<c&&c<d&&d>e){
+  }else if (d>a&&d>b&&d>c&&d>e){
     return 4;
   }return 5;
 }
 void main() {
-  print(func(84635));
+  print(func(84235));
 }
